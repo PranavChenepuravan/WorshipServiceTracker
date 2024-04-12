f9d61b1df9f6ddde96725d6ca640e817d08563bf
Pranav <pranavparathakkad@gmail.com>
2024-04-12 16:03:37 +0530

    API Created for view Pilgrim donation in Institution module

--- src/Institution/InstDonationList.jsx
+++ src/Institution/InstDonationList.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 export const InstDonationList = () => {
     let id=localStorage.getItem('id')
     const [data,setdata]=useState([''])
+    const [data1,setdata1]=useState([''])
     const [refresh,setrefresh]=useState('')
 
     useEffect(()=>{
@@ -11,11 +12,16 @@ export const InstDonationList = () => {
             let response=await axios.get(`http://localhost:4000/institution/donation/${id}`)
             console.log(response.data);
             setdata(response.data)
+
+            let response1=await axios.get(`http://localhost:4000/institution/pilgdonation/${id}`)
+            console.log(response1.data,'response1');
+            setdata1(response1.data)
         }
         fetchdata()
     },[refresh])
   return (
     <>
+     <p className='text-white text-xl' >Offline</p>
      <div className="  overflow-x-auto  ">
       <div className='flex'>
         <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2">Search</button>
@@ -51,9 +57,6 @@ export const InstDonationList = () => {
                 <th scope="col" className="px-6 py-3">
                     Amount
                 </th>
-                <th scope="col" className="px-6 py-3">
-                    Tax
-                </th>
             </tr>
         </thead>
         <tbody>
@@ -86,9 +89,48 @@ export const InstDonationList = () => {
                     <td>
                         {item.donation ? item.donation.amount : "Donation data not avialable"}
                     </td>
-                    <td>
-                        {item.donation ? item.donation.tax : "Donation data not avialable"}
-                    </td>
+
+                </tr>
+            ))}
+            
+        </tbody>
+    </table>
+
+
+
+
+
+<br />
+<p className='text-white text-xl' >Online</p>
+<div className='flex'>
+        <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2">Search</button>
+        <input  type="text" name='time' id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[20%] h-[2%]" required />
+      </div>
+
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    PilgrimId
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Time
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Amount
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {data1.map((item,index)=>(
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td>{item.pilgrimId}</td>
+                    <td>{item.date}</td>
+                    <td>{item.time}</td>
+                    <td>{item.amount}</td>
 
                 </tr>
             ))}
--- src/Pilgrim/PilgDonation.jsx
+++ src/Pilgrim/PilgDonation.jsx
@@ -93,6 +93,10 @@ export const PilgDonation = () => {
     <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Weight(Other)</label>
     <input onChange={handleChange} type="text" name='weight' id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mx-[2%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[95%]"  />
   </div>
+  <div className="mb-5">
+    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Worth(Money)</label>
+    <input onChange={handleChange} type="text" name='worth' id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mx-[2%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[95%]"  />
+  </div>
 
   <div className=' flex items-center gap-[10px] mt-[5%] ml-[39%]'>
   <div><button type="submit" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mx-[90%]">Proceed</button></div>
