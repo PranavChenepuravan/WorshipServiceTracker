a46453e51e867d8403d67e1772cb06d2a64da842
PranavChenepuravan <117366861+PranavChenepuravan@users.noreply.github.com>
2024-05-18 15:29:00 +0530

    Delete src directory

--- src/Component/Instback.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// import pic from '../Institution/Oldpaper.png'
- 
-export const Instback = () => {
-  return (
-    <div><img src={pic} alt="" className='bsolute z-0'/></div>
-  )
-}
-
